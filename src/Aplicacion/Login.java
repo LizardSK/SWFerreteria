fc702ec19436168d2a0f297c399a22275ee23c1e
Marco Antonio Moreno Martínez <lizard_19_m@hotmail.com>
2016-05-19 19:29:33 -0500

    Inicialización del proyecto
    Comienzo de la carpeta del proyecto

---
+++ src/Aplicacion/Login.java
@@ -0,0 +1,23 @@
+package Aplicacion;
+
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener
+{
+	
+	public Login()
+	{	
+	}
+	
+	public static void main(String [] args)
+	{
+		new Login();
+	}
+
+	public void actionPerformed(ActionEvent ae) 
+	{
+	}
+}
